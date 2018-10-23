551b32809f75c5a5a7cc2a8009161f729b0b4473
Aaron Gervasio <aaron.gervasio@gmail.com>
2018-10-23 03:38:21 -0400

    completed

--- src/training_day.clj
+++ src/training_day.clj
@@ -1,6 +1,17 @@
 (ns training-day)
 
-(def answer ":(")
+(def fruit-prices
+  {:apple 0.75,
+   :orange 0.80,
+   :pomegranate 2.50,
+   :banana 0.50,
+   :plum 1.20,
+   :peach 1.00,
+   :persimmon 1.75,
+   :lime 0.60})
+
+
+(def answer 42)
 
 (def hai "O HAI!")
 
@@ -9,3 +20,12 @@
 
 (defn average [a b]
   ":(")
+
+(defn tally-item [prices line-item]
+  (let [name (first line-item)
+        number (second line-item)
+        price (get prices name)]
+    (* price number)
+    )
+  )
+
